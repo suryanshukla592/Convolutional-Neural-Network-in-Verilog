655469083acaa031a3fdc0dd678835ae069e1a17
Suryansh Shukla <suryanshukla9449@gmail.com>
2026-05-16 17:15:56 +0530

    Enhance row counter reset logic in Pooling module
    Added explicit reset for row counter at the end of the image processing.

--- Pooling.v
+++ Pooling.v
@@ -1,22 +1,23 @@
 `timescale 1ns / 1ps
-
+/* no need for signed reg, since the numbers will 
+always be +ve cause of the ReLu Activation function*/
 module Pooling #(parameter Data_size=8, b_size=26)
 (
     input clk,
     input reset,
-    input [Data_size-1:0] pixel_in,
+    input  [Data_size-1:0] pixel_in,
     input valid_in,
-    output reg [Data_size-1:0] out,
+    output reg  [Data_size-1:0] out,
     output reg valid_out
 );
 // Buffer to store the even row 
-reg [Data_size-1:0] Buffer0 [0:b_size-1]; 
+reg  [Data_size-1:0] Buffer0 [0:b_size-1]; 
 // Registers
-reg [Data_size-1:0] previous_odd_pixel, previous_even_pixel;
+reg  [Data_size-1:0] previous_odd_pixel, previous_even_pixel;
 reg [$clog2(b_size):0] row_count, col_count; // Range: 0 to 25
 // Wires for combinational logic
-wire [Data_size-1:0] current_odd_pixel, current_even_pixel;
-wire [Data_size-1:0] max_even, max_odd;
+wire  [Data_size-1:0] current_odd_pixel, current_even_pixel;
+wire  [Data_size-1:0] max_even, max_odd;
 // Current pixels (Right side of the 2x2 window)
 assign current_even_pixel = Buffer0[col_count];
 assign current_odd_pixel  = pixel_in;
@@ -24,7 +25,7 @@ assign current_odd_pixel  = pixel_in;
 assign max_even = (current_even_pixel > previous_even_pixel) ? current_even_pixel : previous_even_pixel;
 assign max_odd  = (current_odd_pixel > previous_odd_pixel) ? current_odd_pixel : previous_odd_pixel;
 // Final max logic combining both rows
-wire [Data_size-1:0] final_max = (max_even > max_odd) ? max_even : max_odd;
+wire  [Data_size-1:0] final_max = (max_even > max_odd) ? max_even : max_odd;
 always @(posedge clk) begin
     if(!reset) begin
         row_count <=0;
@@ -38,7 +39,12 @@ always @(posedge clk) begin
         //Grid Counter Update
         if(col_count == b_size-1) begin
             col_count <= 0;
-            row_count <= row_count + 1;
+            // NEW: Explicitly reset the row counter at the end of the image!
+            if (row_count == b_size-1) begin
+                row_count <= 0; end
+            else begin
+                row_count <= row_count + 1;
+            end
         end
         else begin
             col_count <= col_count + 1;
